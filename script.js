f1e5221852ea867b54e5ea947f966f16c7719eda
SRE-NADH <95708236+SRE-NADH@users.noreply.github.com>
2024-02-06 15:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+let header = document.getElementsByClassName("header")[0];
+let body  = document.getElementsByTagName('body')[0];
+let links = document.getElementsByClassName("links")[0];
+let slide = document.getElementsByClassName("slide")[0];
+
+
+window.addEventListener('resize',handleResize);
+
+function handleResize(e){
+	if(window.innerWidth<=480){
+		
+		header.innerHTML = `
+		       <p>Hamburger Navigation</p>
+	            <div>
+				<p onClick="show()" >click event</P>
+				</div> `;
+		console.log(header);
+	}
+	else{
+				
+		header.innerHTML = `
+		       <p>Hamburger Navigation</p>
+	           <div class="links" >
+		  <p>Home</p> 
+		  <p>BLog</p> 
+		  <p>about me</p> 
+	   </div> `;
+		
+	 slide.setAttribute('active','false');
+	}
+	
+}
+
+
+function show(){
+	slide.setAttribute('active','true');
+}
